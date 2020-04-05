4ce3020db31fed618203fb049bc4dada34cfc0ff
Danish Ali <1712danish>
2020-04-06 01:39:27 +0530

    second commit

--- os_Assingment.c
+++ os_Assingment.c
@@ -1,6 +1,7 @@
 #include<stdio.h>
 #include<conio.h>
 void main()
+//73886MsMsAm13058
 {
   char p[10][5],temp[5];
   int i,j,pt[10],wt[10],totwt=0,tat[10],pr[10],temp1,n;
@@ -15,6 +16,24 @@ void main()
  scanf("%d",&pt[i]);
   printf("enter priority:");
   scanf("%d",&pr[i]);
+  }
+   for(i=0;i<n-1;i++)
+ {
+ for(j=i+1;j<n;j++)
+ {
+   if(pr[i]>pr[j])
+ {
+   temp1=pr[i];
+ pr[i]=pr[j];
+  pr[j]=temp1;
+  temp1=pt[i];
+  pt[i]=pt[j];
+  pt[j]=temp1;
+ strcpy(temp,p[i]);
+ strcpy(p[i],p[j]);
+  strcpy(p[j],temp);
+  }
+  }
   }
   
 }
